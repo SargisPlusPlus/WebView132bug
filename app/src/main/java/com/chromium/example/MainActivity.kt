aa82ad5cd725f1eacc746521837f4f17764aba40
Sargis <sargis@eluve.com>
2025-02-03 19:45:49 -0800

    simple app to reproduce Webview 132 bug

---
+++ app/src/main/java/com/chromium/example/MainActivity.kt
@@ -0,0 +1,256 @@
+package com.chromium.example
+
+import android.app.ComponentCaller
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.ViewGroup
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : ComponentActivity() {
+
+    private val REQUEST_MICROPHONE = 1
+    private val REQUEST_MANAGE_STORAGE = 2
+
+    fun getWebViewVersion() {
+        val webViewVersion = try {
+            WebView.getCurrentWebViewPackage()?.versionName
+        } catch (e: Exception) {
+            Log.e("WebViewVersion", "Error getting WebView version", e)
+            null
+        }
+
+        if (webViewVersion != null) {
+            Log.d("WebViewVersion", "WebView version: $webViewVersion")
+        } else {
+            Log.d("WebViewVersion", "WebView version: Not available")
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                WebViewScreen()
+            }
+        }
+
+        // Check and request microphone permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), REQUEST_MICROPHONE)
+        }
+/*
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:$packageName"))
+                startActivityForResult(intent, REQUEST_MANAGE_STORAGE)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ), REQUEST_MANAGE_STORAGE)
+            }
+        }*/
+        getWebViewVersion()
+    }
+
+    @Suppress("ControlFlowWithEmptyBody")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+        if (requestCode == REQUEST_MICROPHONE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // TODO Permission granted
+            } else {
+                // TODO Permission denied
+            }
+        }
+
+        if (requestCode == REQUEST_MANAGE_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // Permissions granted
+            } else {
+                // Permissions denied
+            }
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        caller: ComponentCaller
+    ) {
+        super.onActivityResult(requestCode, resultCode, data, caller)
+        if (requestCode == REQUEST_MANAGE_STORAGE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    // Permission granted
+                } else {
+                    // Permission denied
+                }
+            }
+        }
+    }
+
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == REQUEST_MICROPHONE) {
+//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//                // Permission granted
+//            } else {
+//                // Permission denied
+//            }
+//        }
+//    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable 
+fun WebViewScreen() {
+    // CHANGE THIS TO TEST OUT WITH DIFFERENT URLS
+    var url by remember { mutableStateOf("https://jsbin.com/zikimemoro/edit?html,js,output") }
+    var webView: WebView? = null
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AndroidView(factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+
+                WebViewUniversal(this)
+
+                loadUrl(url)
+
+                webViewClient = object : WebViewClient() {
+
+                    override fun shouldOverrideUrlLoading(
+                        view: WebView?,
+                        request: WebResourceRequest?
+                    ): Boolean {
+                        view?.loadUrl(request?.url.toString())
+                        return super.shouldOverrideUrlLoading(view, request)
+                    }
+
+                    override fun onPageStarted(
+                        view: WebView?,
+                        url: String?,
+                        favicon: Bitmap?
+                    ) {
+                        super.onPageStarted(view, url, favicon)
+                    }
+
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                    }
+
+                    override fun onReceivedError(
+                        view: WebView?,
+                        request: WebResourceRequest?,
+                        error: WebResourceError?
+                    ) {
+                        super.onReceivedError(view, request, error)
+                    }
+                }
+
+                // Set web view chrome client
+                webChromeClient = object : WebChromeClient() {
+
+                    override fun onPermissionRequest(request: PermissionRequest?) {
+                        request?.grant(request.resources)
+                    }
+
+                    override fun onProgressChanged(
+                        view: WebView, newProgress: Int
+                    ) {
+                    }
+                }
+
+            }
+        }, update = {
+            it.loadUrl(url)
+        }, modifier = Modifier.fillMaxSize())
+
+    }
+}
+
+class WebViewUniversal(webView: WebView) {
+    init {
+        webView.apply {
+            this.
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            settings.mediaPlaybackRequiresUserGesture = false
+            settings.allowFileAccessFromFileURLs = true
+            settings.allowUniversalAccessFromFileURLs = true
+//            clearCache(true)
+            clearHistory()
+            settings.allowContentAccess = true
+            settings.domStorageEnabled = true
+            settings.builtInZoomControls = true
+            settings.supportZoom()
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.displayZoomControls = true
+            settings.allowFileAccess = true
+            settings.safeBrowsingEnabled = false
+        }
+    }
+}
